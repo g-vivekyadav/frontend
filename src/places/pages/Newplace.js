dfef320b895ac518874430096947200eac2a88ec
Vivek <vivekyadavgorla@gmail.com>
2023-02-01 21:18:10 +0530

    committ

--- src/places/pages/Newplace.js
+++ src/places/pages/Newplace.js
@@ -1,5 +1,4 @@
 //git status checkout
-<<<<<<<<< Temporary merge branch 1
 // Testing file for git husb
 let y=0;
 let z=0;
@@ -9,3 +8,4 @@ console.log(x)
 console.log("hello")
 console.log("new")
 console.log("hi")
+console.log("testing")
